e33a4e5f1b4d30b664919fd000e839cc98b9e968
wngai01 <mongaiwaiyin@gmail.com>
2025-01-18 23:58:39 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Test from "./Test.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Calender from "./pages/Calendar.jsx";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
