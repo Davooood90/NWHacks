bdb8a0588bc36bbac0cb315f8a96de908fb798e2
wngai01 <mongaiwaiyin@gmail.com>
2025-01-18 16:07:37 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Test from './Test.jsx'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+<<<<<<< Updated upstream
       <App />
+=======
+    {/* <App /> */}
+    <Test />
+>>>>>>> Stashed changes
   </StrictMode>,
 )
