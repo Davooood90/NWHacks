49435e1a553fe996f6937b9c2ac9154b6d37c2e9
wngai01 <mongaiwaiyin@gmail.com>
2025-01-19 03:52:35 -0800

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,26 @@ import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
 import AppPage from "./pages/AppPage";
+import { Navigate } from 'react-router-dom';
+
 
 function App() {
+
+  const ProtectedRoute = ({ element }) => {
+    return sessionStorage.getItem("userEmail") ? element : <Navigate to="/login" />;
+  };
+
   return (
     <GoogleOAuthProvider clientId="405080598434-go6bo0850qf7ebno0lstqad1f8mi37ff.apps.googleusercontent.com">
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/app" element={<AppPage />} />
+
+        <Route 
+          path="/app" 
+          element={<ProtectedRoute element={<AppPage />} />} 
+        />
       </Routes>
     </GoogleOAuthProvider>
   );
